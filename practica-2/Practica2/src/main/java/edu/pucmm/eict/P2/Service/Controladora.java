c33af9f10629a784142a80cfc999a893a7776377
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-06-14 19:05:55 -0400

    Agregando metodos para controlar productos

--- practica-2/Practica2/src/main/java/edu/pucmm/eict/P2/Service/Controladora.java
+++ practica-2/Practica2/src/main/java/edu/pucmm/eict/P2/Service/Controladora.java
@@ -1,5 +1,62 @@
 package edu.pucmm.eict.P2.Service;
 
+import edu.pucmm.eict.P2.Logico.Producto;
+import edu.pucmm.eict.P2.Logico.Usuario;
+import edu.pucmm.eict.P2.Logico.VentaProductos;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controladora {
+    private static Controladora instancia;
+    private List<Usuario> listaUsuarios = new ArrayList<>();
+    private List<Producto> listaProductos = new ArrayList<>();
+    private List<VentaProductos> listaVentas = new ArrayList<>();
+
+    private static int contadorProductos = 1;
+
+    private Controladora(){
+        listaUsuarios.add(new Usuario("admin","admin","admin"));
+    }
+
+    public static Controladora getInstance(){
+        if (instancia == null){
+            instancia = new Controladora();
+        }
+        return instancia;
+    }
+
+    public Producto agregarProducto(Producto producto){
+
+        producto.setIdProducto(contadorProductos);
+        listaProductos.add(producto);
+
+        contadorProductos += 1;
+
+        return producto;
+    }
+
+
+    public Producto eliminarProducto(int id){
+        Producto tmp = buscarProductoPorId(id);
+
+        if (tmp != null){
+            listaProductos.remove(tmp);
+        }
+
+        return tmp;
+    }
+
+    public Producto buscarProductoPorId(int id){
+
+
+        for (Producto p : listaProductos){
+            if (p.getIdProducto() == id){
+                return p;
+            }
+        }
+        return null;
+    }
 
 }
