502f4eb8f59d9890cd82282bfa407a9b5e6cc7ee
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-06-28 15:29:11 -0400

    Arreglando entidad comentario

---
+++ practica-3/Practica3/src/main/java/edu/pucmm/eict/P3/Entidades/Comentario.java
@@ -0,0 +1,55 @@
+package edu.pucmm.eict.P3.Entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Comentario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String contenido;
+    private String usuario;
+    @ManyToOne
+    private Producto producto;
+
+    public Comentario() {
+    }
+
+    public Comentario(String contenido, String usuario, Producto producto) {
+        this.contenido = contenido;
+        this.usuario = usuario;
+        this.producto = producto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
