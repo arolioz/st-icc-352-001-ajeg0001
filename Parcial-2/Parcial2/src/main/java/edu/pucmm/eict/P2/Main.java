c9cd1cecbcd37ac77d8d8bbcc9125b94751a17d9
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-07-14 22:41:40 -0400

    Agregando la conexion a la base de datos h2 en modo servidor

--- Parcial-2/Parcial2/src/main/java/edu/pucmm/eict/P2/Main.java
+++ Parcial-2/Parcial2/src/main/java/edu/pucmm/eict/P2/Main.java
@@ -1,14 +1,19 @@
 package edu.pucmm.eict.P2;
 
+import edu.pucmm.eict.P2.Services.BootStrapServices;
 import io.javalin.Javalin;
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-
-import java.util.Properties;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
 
 public class Main {
     void main() {
+
+        BootStrapServices.getInstancia().init();
+
+        EntityManager em = Persistence
+                .createEntityManagerFactory("MiUnidadPersistencia")
+                .createEntityManager();
+
         var app = Javalin.create(config ->{
             config.routes.get("/", ctx ->{
                 ctx.result("Hola mundo!");
