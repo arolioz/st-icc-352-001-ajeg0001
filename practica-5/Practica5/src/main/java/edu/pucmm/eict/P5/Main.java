06762914cb9f9e21c2ead61b2b91b6d6cd8c2420
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-07-14 22:25:56 -0400

    Fix

--- practica-5/Practica5/src/main/java/edu/pucmm/eict/P5/Main.java
+++ practica-5/Practica5/src/main/java/edu/pucmm/eict/P5/Main.java
@@ -54,7 +54,6 @@ public class Main {
                             actualizarCantUsuariosConectados();
                         });
             });
-
             config.routes.ws("/comentarios", ws -> {
 
                         ws.onConnect(ctx -> {
