2611eaf23f046857986efa676f4576b4b597f9dd
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-07-14 23:24:40 -0400

    Creando Usuario Admin por defecto

--- Parcial-2/Parcial2/src/main/java/edu/pucmm/eict/P2/Main.java
+++ Parcial-2/Parcial2/src/main/java/edu/pucmm/eict/P2/Main.java
@@ -1,9 +1,15 @@
 package edu.pucmm.eict.P2;
 
+import Util.RolesApp;
+import edu.pucmm.eict.P2.Entidades.Usuario;
 import edu.pucmm.eict.P2.Services.BootStrapServices;
+import edu.pucmm.eict.P2.Services.UsuarioServices;
 import io.javalin.Javalin;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
+
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     void main() {
@@ -19,6 +25,25 @@ public class Main {
                 ctx.result("Hola mundo!");
             });
         });
+
+        crearDatosIniciales();
         app.start(7000);
+    }
+
+    public static void crearDatosIniciales(){
+        List<Usuario> usuarios = UsuarioServices.getInstancia().findAll();
+
+        if (usuarios.isEmpty()){
+            Usuario u = new Usuario();
+
+            u.setNombre("admin");
+            u.setUsuario("admin");
+            u.setPassword("admin");
+
+            u.setListaRoles(Set.of(RolesApp.ROLE_ADMIN,RolesApp.ROLE_ORGANIZADOR,RolesApp.ROLE_USUARIO));
+
+            UsuarioServices.getInstancia().crear(u);
+        }
+
     }
 }
