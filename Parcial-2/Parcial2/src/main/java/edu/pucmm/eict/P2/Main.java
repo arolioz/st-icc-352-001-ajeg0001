7eff23705e022266f45f44556b65534767a8ad46
AARON JOSÉ ESCAÑO GARCÍA <arolio301911@gmail.com>
2026-07-14 22:28:31 -0400

    Agregando configuracion de persistencia

--- Parcial-2/Parcial2/src/main/java/edu/pucmm/eict/P2/Main.java
+++ Parcial-2/Parcial2/src/main/java/edu/pucmm/eict/P2/Main.java
@@ -1,15 +1,19 @@
 package edu.pucmm.eict.P2;
 
 import io.javalin.Javalin;
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
 
 public class Main {
-    void main(){
+    void main() {
         var app = Javalin.create(config ->{
             config.routes.get("/", ctx ->{
                 ctx.result("Hola mundo!");
             });
         });
-
         app.start(7000);
     }
 }
