ad23849763ac4252894ccfc1f5093f8cc94db5a8
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-08-19 18:41:38 -0400

    Creando metodo para autenticar el usuario

--- proyecto-final/ProyectoFinal/src/main/resources/Publico/js/login.js
+++ proyecto-final/ProyectoFinal/src/main/resources/Publico/js/login.js
@@ -1,11 +1,11 @@
 (() => {
-    console.log("ENTRA AL SCRIPT LOGIN")
+    console.log("ENTRA AL SCRIPT LOGIN");
 
     function procesarLogin() {
 
         const loginForm = document.getElementById('loginForm');
 
-        loginForm.addEventListener('submit', (event) => {
+        loginForm.addEventListener('submit', async (event) => {
 
             event.preventDefault();
 
@@ -15,6 +15,8 @@
             if (!validarDatos(usuario, password)) {
                 return;
             }
+
+            await autenticarUsuario(usuario, password);
 
         });
     }
@@ -31,6 +33,32 @@
         }
 
         return true;
+    }
+
+    async function autenticarUsuario(usuario, password) {
+
+         try {
+            const data = { token: "123456", usuario: hola};
+            const respuesta = await fetch('/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({usuario,password})
+            });
+
+            if (respuesta.ok) {
+                const data = await respuesta.json();
+
+                console.log(data);
+
+                console.log(localStorage.setItem("token", data.token));
+                console.log(localStorage.setItem("usuario", data.usuario));
+
+                window.location.href = 'formulario.html';
+
+            }
+        } catch (e) {
+            console.error("Error al conectar con el servidor");
+        }
     }
 
     procesarLogin();
