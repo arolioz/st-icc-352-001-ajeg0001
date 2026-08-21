01d4054a8b4464516855a0d494ab6cf0ca09e134
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-08-21 13:31:07 -0400

    Agregando el metodo para guardar los formularios

--- proyecto-final/ProyectoFinal/src/main/resources/Publico/js/formulario.js
+++ proyecto-final/ProyectoFinal/src/main/resources/Publico/js/formulario.js
@@ -1,6 +1,11 @@
 (() => {
     console.log("ENTRA AL SCRIPT FORMULARIO");
 
+    if (!validarSesion()) {
+        window.location.href = "login.html";
+        return;
+    }
+
     const btnNivelEscolar = document.getElementById("btnNivelEscolar");
     const opciones = document.querySelectorAll(".dropdown-item");
     const idNivelEscolar = document.getElementById("idNivelEscolar");
@@ -21,6 +26,52 @@
         });
     }
 
+    function procesarFormulario() {
+
+        const idForm = document.getElementById('idForm');
+
+        idForm.addEventListener('submit', async (event) => {
+
+            event.preventDefault();
+
+            const nombre = document.getElementById("idNombre").value;
+            const sector = document.getElementById('idSector').value;
+            const nivelEscolar = document.getElementById('idNivelEscolar').value;
+
+            if (!nombre || !sector || !nivelEscolar) {
+                alert("Todos los campos son obligatorios");
+                return;
+            }
+
+            guardarFormulario(nombre, sector, nivelEscolar);
+
+        });
+    }
+
+    function guardarFormulario(nombre, sector, nivelEscolar) {
+
+        const formularios = JSON.parse(localStorage.getItem("formularios")) || [];
+
+        const nuevoForm = {
+            uuid: crypto.randomUUID(),
+            nombre: nombre,
+            sector: sector,
+            nivelEscolar: nivelEscolar,
+            usuarioId: localStorage.getItem("id"),
+            usuario: localStorage.getItem("usuario")
+        };
+
+        formularios.push(nuevoForm);
+        localStorage.setItem("formularios", JSON.stringify(formularios));
+
+        //PRUEBA
+        console.log(JSON.parse(localStorage.getItem("formularios")));
+
+        alert("Formulario guardado correctamente");
+
+    }
+
     configurarDropdown();
+    procesarFormulario();
 
 })();
