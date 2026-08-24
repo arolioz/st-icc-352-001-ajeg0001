d2b88903255aba089e6cba49a5031118e60f866d
Elianny Urena <eliannyurenafdez17@gmail.com>
2026-08-24 12:13:31 -0400

    Creando metodos para mostrar el mapa de los registros

---
+++ proyecto-final/ProyectoFinal/src/main/resources/Publico/js/mapaRegistros.js
@@ -0,0 +1,66 @@
+(() => {
+
+    console.log("Entrando al Mapa");
+
+    const idMapa = L.map("idMapa").setView([19.425299, -70.712168], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="colaboradoreshttps://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(idMapa);
+
+    async function cargarFormularios() {
+
+        try {
+
+            const respuesta = await fetch("/api/encuesta", {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("token"),
+                    "Accept": "application/json"
+                }
+            });
+
+            if (!respuesta.ok) {
+                throw new Error(`HTTP error! Status: ${respuesta.status}`);
+            }
+
+            const formularios = await respuesta.json();
+
+            console.log("Formularios:", formularios);
+
+            mostrarMapa(formularios);
+
+        } catch (error) {
+
+            console.error("Error cargando formularios:", error);
+
+        }
+
+    }
+
+    function mostrarMapa(formularios) {
+
+        const coordenadas = [];
+        formularios.forEach( form => {
+
+           if (form.latitud == null || form.longitud == null) {
+               return;
+           }
+
+           const coordenada = [form.latitud, form.longitud];
+
+           coordenadas.push(coordenada);
+
+           const ubicacion = L.marker([form.latitud, form.longitud]).addTo(idMapa);
+
+           ubicacion.bindPopup(`<strong>${form.nombre}</strong>
+                                <p>Registrado por: ${form.usuarioNombre}</p>`);
+
+        });
+
+        if (coordenadas > 0) {
+            idMapa.fitBounds(coordenadas);
+        }
+
+    }
+
+    cargarFormularios();
+
+})();
